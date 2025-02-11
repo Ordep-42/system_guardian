d14c8ceeab513d6bacf75ecad893d9de52277781
Ordep-42 <90785456+Ordep-42@users.noreply.github.com>
2025-02-11 15:13:58 -0300

    feat: Versão básica do menu baseada na atividade da unidade 6.

--- main.c
+++ main.c
@@ -1,47 +1,127 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "pico/time.h"
+#include "inc/ssd1306.h"
 #include "hardware/i2c.h"
-#include "hardware/timer.h"
-#include "hardware/clocks.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+const uint I2C_SDA = 14;
+const uint I2C_SCL = 15;
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
+uint8_t seletor;
+bool on_menu = true;
+uint32_t ms_since_last_IRQ = 0;
+uint8_t IRQ_delay = 70;
+
+void IRQ_callback() {
+    if ((uint32_t)(to_ms_since_boot(get_absolute_time()) - ms_since_last_IRQ) >= IRQ_delay) {
+        on_menu = !on_menu;
+        printf("IRQ!\n");
+        ms_since_last_IRQ = to_ms_since_boot(get_absolute_time());
+    }
 }
 
-
-
-
-int main()
-{
-    stdio_init_all();
-
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
+void init_display() {
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+}
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+void init_joystick() {
+    adc_init();
+    adc_gpio_init(26);
+}
 
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+void display_menu(uint8_t* ssd, struct render_area* frame_area, uint8_t select) {
+    char *menu[] = {
+        " Temp e Umid  ",
+        " Consumo Ener ",
+        " LED RGB      "};
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    int y = 0;
+    char text[17];
+
+    for (uint i = 0; i < count_of(menu); i++) {
+        if (i == select) {
+            strcpy(text, ">");
+        } else {
+            strcpy(text, " ");
+        }
+        strcat(text, menu[i]);
+        ssd1306_draw_string(ssd, 5, y, text);
+        y += 8;
     }
+    render_on_display(ssd, frame_area);
+}
+
+int main()
+{
+    stdio_init_all();
+    
+    init_joystick();
+    init_display();
+    ssd1306_init();
+
+    gpio_init(22);
+    gpio_set_dir(22, GPIO_IN);
+    gpio_pull_up(22);
+    gpio_set_irq_enabled_with_callback(22, GPIO_IRQ_EDGE_FALL, true, &IRQ_callback);
+
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    uint8_t counter = 0;
+    for (;;) {
+        if (on_menu) {
+            printf("Menu!\n");
+            adc_select_input(0);
+            uint16_t x = adc_read();
+
+            if (x < 1365) {
+                counter++;
+            } else if (x > 4095 - 1365) {
+                counter--;
+            }
+
+            seletor = counter % 3;
+
+            display_menu(ssd, &frame_area, seletor);
+        } else if (!on_menu) { 
+            switch (seletor) {
+                case 0:
+                    // Visualizar temperatura e umidade
+                    printf("Temperatura e umidade\n");
+                    break;
+
+                case 1:
+                    // Visualizar consumo energético
+                    printf("Consumo energético\n");
+                    break;
+
+                case 2:
+                    // Sla
+                    printf("Terceira opção\n");
+                    break;
+            }
+        }
+        sleep_ms(100);
+    }
+    return 0;
 }

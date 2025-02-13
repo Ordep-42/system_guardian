8f52bfe694b7c6d2ab189a62ec50e8df45912d79
Ordep-42 <90785456+Ordep-42@users.noreply.github.com>
2025-02-13 12:21:50 -0300

    fix: Typos na definição da função led_set_color().

--- inc/led_rgb/led.c
+++ inc/led_rgb/led.c
@@ -7,7 +7,7 @@ void led_init(uint8_t pin) {
     gpio_set_dir(pin, GPIO_OUT);
 }
 
-void led_set_colors(uint8_t red_pin, uint8_t green_pin, uint8_t blue_pin, bool red_state, bool green_state, bool blue_state) {
+void led_set_color(uint8_t red_pin, uint8_t green_pin, uint8_t blue_pin, bool red_state, bool green_state, bool blue_state) {
     gpio_put(red_pin, red_state);
     gpio_put(green_pin, green_state);
     gpio_put(blue_pin, blue_state);
--- inc/led_rgb/led.h
+++ inc/led_rgb/led.h
@@ -1,4 +1,8 @@
 #pragma once
 
+#define LED_R_PIN 13
+#define LED_G_PIN 11
+#define LED_B_PIN 12
+
 void led_init(uint8_t pin);
 void led_set_color(uint8_t red_pin, uint8_t green_pin, uint8_t blue_pin, bool red_state, bool green_state, bool blue_state);

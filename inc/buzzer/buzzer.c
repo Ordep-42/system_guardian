0f1699f29152a093f2f8057deb874907e757a112
Ordep-42 <90785456+Ordep-42@users.noreply.github.com>
2025-02-13 12:21:02 -0300

    fix: Correção na função de inicialização do buzzer pra não incluir a frequência.

--- inc/buzzer/buzzer.c
+++ inc/buzzer/buzzer.c
@@ -3,19 +3,19 @@
 #include "hardware/pwm.h"
 #include "buzzer.h"
 
-void buzzer_init(uint8_t pin, uint frequency_hz) {
+void buzzer_init(uint8_t pin) {
     gpio_set_function(pin, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(pin);
     
     pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, clock_get_hz(clk_sys) / (frequency_hz * 4096));
+    pwm_config_set_clkdiv(&config, 4.0f);
     pwm_init(slice_num, &config, true);
 
     pwm_set_gpio_level(pin, 0);
 }
 
 void beep(uint8_t pin, uint duration_ms) {
-    pwm_set_gpio_level(pin, 1);
+    pwm_set_gpio_level(pin, 2048);
     sleep_ms(duration_ms);
     pwm_set_gpio_level(pin, 0);
 }
--- inc/buzzer/buzzer.h
+++ inc/buzzer/buzzer.h
@@ -1,4 +1,4 @@
 #pragma once
 
-void buzzer_init(uint8_t pin, uint frequency_hz);
+void buzzer_init(uint8_t pin);
 void beep(uint8_t pin, uint duration_ms);

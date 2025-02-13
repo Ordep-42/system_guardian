ca1824f16e7f9d6a313b85e9ea606b41fbcc598c
Ordep-42 <90785456+Ordep-42@users.noreply.github.com>
2025-02-13 16:09:20 -0300

    feat: Código do sensor de energia simulado via potenciometro. Implementado e testado.

--- main.c
+++ main.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
 #include "inc/aht10/aht.h"
 #include "inc/alarm/alarm.h"
 #include "inc/led_rgb/led.h"
 
-void termo_task(void *pvParameters)
+#define POWER_METER_PIN 28
+#define POWER_METER_ADC 2
+
+void sensor_task(void *pvParameters)
 {
     aht_dev_t aht;
     aht_init(&aht, i2c0, AHT10_ADDRESS, 16, 17);
 
+    adc_init();
+    adc_gpio_init(POWER_METER_PIN);
+
+    uint16_t power_draw;
     aht_data_t data;
+
     for (;;) {
+        adc_select_input(POWER_METER_ADC);
+        power_draw = adc_read() / 1.3;
+
         if (aht_read_data(&aht, &data) == PICO_ERROR_NONE) {
             printf("Temperature: %.2f C\n", data.temperature);
             printf("Humidity: %.2f %%\n", data.humidity);
-            if (data.temperature >= 40 || data.humidity >= 90) {
+            printf("Power: %d\n", power_draw);
+            if (data.temperature >= 40 || data.humidity >= 90 || power_draw >= 2800) {
                 alarm_trigger(true, false, false, 300);
-            } else if ((data.temperature >= 30 && data.temperature < 40) || (data.humidity < 90 && data.humidity >= 80)) {
+            } else if ((data.temperature >= 30 && data.temperature < 40) || (data.humidity < 90 && data.humidity >= 80) || (power_draw >= 2000 && power_draw < 2800)) {
                 led_set_color(LED_R_PIN, LED_G_PIN, LED_B_PIN, true, true, false);
             } else {
                 led_set_color(LED_R_PIN, LED_G_PIN, LED_B_PIN, false, true, false);
@@ -34,5 +48,5 @@ int main()
     alarm_init();
     alarm_arm();
 
-    termo_task(NULL);
+    sensor_task(NULL);
 }
